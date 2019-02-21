f5dc99e03d26d0660f0c5c0eaa8455aca23aaebd
naturling_crifan <crifan.li@naturling.com>
2019-02-21 14:49:57 +0800

    1. update sendFile for python support range 206

--- crifanLib/crifanFlask.py
+++ crifanLib/crifanFlask.py
@@ -15,6 +15,7 @@ __copyright__ = "Copyright (c) 2019, Crifan Li"
 __license__ = "GPL"
 
 import io
+import re
 from flask import send_file
 
 ################################################################################
@@ -45,14 +46,16 @@ gConst = {
 ################################################################################
 
 
-def sendFile(fileBytes, contentType, outputFilename, asAttachment=True):
+
+def sendFile(fileBytes, contentType, outputFilename, asAttachment=True, range=None):
     """
         flask return downloadable file or file's binary stream data
             example url: http://127.0.0.1:34800/audio/5c1c631c127588257d568eba/3569.mp3
     :param fileBytes:  file binary bytes
     :param contentType: MIME content type, eg: audio/mpeg
     :param outputFilename: output filename, eg: 3569.mp3
-    :param asAttachment: True to return downloable file with filename, False to return binary stream file data
+    :param asAttachment: True to return downloadable file with filename, False to return binary stream file data
+    :param range: range header to support 206=Partial Content, eg: bytes=1146880-
     :return: Flask response
     """
     """Flask API use this to send out file (to browser, browser can directly download file)"""
@@ -63,15 +66,65 @@ def sendFile(fileBytes, contentType, outputFilename, asAttachment=True):
     #     as_attachment=asAttachment,
     #     attachment_filename=outputFilename
     # )
-    fileLength = len(fileBytes)
+    respBytes = fileBytes
+    respStatusCode = 200
+
+    totalLen = len(fileBytes)
+    respContentLength = totalLen
+
+    contentRange = None
+    # support range header of 206=Partial Content to support api caller to continue download or audio drag to play
+    if range:
+        # responseFile.status_code = 206 # Partial Content
+        # bytes=1146880-
+        foundBytes = re.search("bytes=(?P<byteStart>\d+)(-(?P<byteEnd>\d+)?)?", range)
+        if foundBytes:
+            byteStart = foundBytes.group("byteStart")
+            # if byteStart:
+            byteStart = int(byteStart)
+            # for debug
+            # if byteStart >= 0:
+            if byteStart > 0:
+                respStatusCode = 206
+
+            byteEnd = foundBytes.group("byteEnd")
+            if byteEnd:
+                byteEnd = int(byteEnd)
+                if byteEnd > 0:
+                    respStatusCode = 206
+            else:
+                byteEnd = totalLen - 1
+
+            byteEndIdx = byteEnd + 1
+            respBytes = fileBytes[byteStart:byteEndIdx]
+            respContentLength = len(respBytes)
+
+            # print("byteStart=%d, byteEnd=%d, respContentLength=%s" % (byteStart, byteEnd, respContentLength))
+            # Content-Range: bytes 2162688-4010305/4010306
+            contentRange = "bytes %d-%d/%d" % (byteStart, byteEnd, totalLen)
+
     responseFile = send_file(
-        io.BytesIO(fileBytes),
+        # io.BytesIO(fileBytes),
+        io.BytesIO(respBytes),
         mimetype=contentType,
         as_attachment=asAttachment,
-        attachment_filename=outputFilename
+        attachment_filename=outputFilename,
+        # conditional=True # Note: after test conditional is not work well, so implement 206 by myself
     )
+
+    responseFile.headers["Connection"] = "keep-alive"
+    # to support audio miniprogram first seek and drag to play
+    # if contentType == "audio/mpeg":
+    responseFile.headers["Accept-Ranges"] = "bytes"
+
     # add Content-Length to support miniprogram iOS background play audio works, not error: 10003
-    responseFile.headers["Content-Length"] = fileLength
+    responseFile.headers["Content-Length"] = respContentLength
+
+    # if respStatusCode != 200:
+    responseFile.status_code = respStatusCode
+    if contentRange:
+        responseFile.headers["Content-Range"] = contentRange
+
     return responseFile
 
 
