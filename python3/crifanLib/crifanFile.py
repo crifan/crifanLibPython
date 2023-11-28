bd6682e5b088e0817dcafb22cdca729bb0fc92aa
crifanli <admin@crifan.com>
2023-11-28 23:21:01 +0800

    1. update indent

--- python3/crifanLib/crifanFile.py
+++ python3/crifanLib/crifanFile.py
@@ -275,24 +275,24 @@ def unzipFile(zipFileFullPath, outputFolder):
         zip_ref.extractall(outputFolder)
 
 def zipFolder(toZipFolder, outputZipFile):
-  """
-    zip/compress a whole folder/directory to zip file
-  """
-  print("Zip for foler %s" % toZipFolder)
-  with zipfile.ZipFile(outputZipFile, 'w', zipfile.ZIP_DEFLATED) as zipFp:
-    for dirpath, dirnames, filenames in os.walk(toZipFolder):
-      # print("%s" % ("-"*80))
-      # print("dirpath=%s, dirnames=%s, filenames=%s" % (dirpath, dirnames, filenames))
-      # print("Folder: %s, Files: %s" % (dirpath, filenames))
-      for curFileName in filenames:
-        # print("curFileName=%s" % curFileName)
-        curFilePath = os.path.join(dirpath, curFileName)
-        # print("curFilePath=%s" % curFilePath)
-        fileRelativePath = os.path.relpath(curFilePath, toZipFolder)
-        # print("fileRelativePath=%s" % fileRelativePath)
-        # print("  %s" % fileRelativePath)
-        zipFp.write(curFilePath, arcname=fileRelativePath)
-  print("Completed zip file %s" % outputZipFile)
+    """
+        zip/compress a whole folder/directory to zip file
+    """
+    print("Zip for foler %s" % toZipFolder)
+    with zipfile.ZipFile(outputZipFile, 'w', zipfile.ZIP_DEFLATED) as zipFp:
+        for dirpath, dirnames, filenames in os.walk(toZipFolder):
+            # print("%s" % ("-"*80))
+            # print("dirpath=%s, dirnames=%s, filenames=%s" % (dirpath, dirnames, filenames))
+            # print("Folder: %s, Files: %s" % (dirpath, filenames))
+            for curFileName in filenames:
+                # print("curFileName=%s" % curFileName)
+                curFilePath = os.path.join(dirpath, curFileName)
+                # print("curFilePath=%s" % curFilePath)
+                fileRelativePath = os.path.relpath(curFilePath, toZipFolder)
+                # print("fileRelativePath=%s" % fileRelativePath)
+                # print("  %s" % fileRelativePath)
+                zipFp.write(curFilePath, arcname=fileRelativePath)
+    print("Completed zip file %s" % outputZipFile)
 
 
 ################################################################################
