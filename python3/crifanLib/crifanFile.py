7857e2ca4c287f543823cdc6c9d7a3d212a04f8c
crifanli <admin@crifan.com>
2023-11-28 23:08:38 +0800

    1. add zipFolder

--- python3/crifanLib/crifanFile.py
+++ python3/crifanLib/crifanFile.py
@@ -274,6 +274,26 @@ def unzipFile(zipFileFullPath, outputFolder):
     with zipfile.ZipFile(zipFileFullPath, 'r') as zip_ref:
         zip_ref.extractall(outputFolder)
 
+def zipFolder(toZipFolder, outputZipFile):
+  """
+    zip/compress a whole folder/directory to zip file
+  """
+  print("Zip for foler %s to file %s" % (toZipFolder, outputZipFile))
+  with zipfile.ZipFile(outputZipFile, 'w', zipfile.ZIP_DEFLATED) as zipFp:
+    for dirpath, dirnames, filenames in os.walk(toZipFolder):
+      # print("%s" % ("-"*80))
+      # print("dirpath=%s, dirnames=%s, filenames=%s" % (dirpath, dirnames, filenames))
+      # print("Folder: %s, Files: %s" % (dirpath, filenames))
+      for curFileName in filenames:
+        # print("curFileName=%s" % curFileName)
+        curFilePath = os.path.join(dirpath, curFileName)
+        # print("curFilePath=%s" % curFilePath)
+        fileRelativePath = os.path.relpath(curFilePath, toZipFolder)
+        # print("fileRelativePath=%s" % fileRelativePath)
+        # print("  %s" % fileRelativePath)
+        zipFp.write(curFilePath, arcname=fileRelativePath)
+  print("Complete zip foler %s to file %s" % (toZipFolder, outputZipFile))
+
 
 ################################################################################
 # Folder Function
